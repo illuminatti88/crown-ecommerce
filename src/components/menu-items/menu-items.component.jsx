a78e3ac806c7188d1beaf67f91187d054b515331
illuminatti88 <immortals.300@gmail.com>
2019-07-17 07:34:26 +0530

    splitted homepage into smaller components

---
+++ src/components/menu-items/menu-items.component.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './menu-items.styles.scss';
+
+const MenuItems = ({title, imageUrl, size}) => (
+    <div  className={`${size} menu-item` }>
+      <div className='background-image' style={{backgroundImage:`url(${imageUrl})`}}></div>
+    <div className='content'>
+      <div className='title'>{title.toUpperCase()}</div>
+      <span className='subtitle'>SHOP NOW</span>
+    </div>
+  </div>
+)
+
+export default MenuItems;
